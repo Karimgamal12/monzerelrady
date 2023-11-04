8e3244f985aa2617a4a209f368ade13d3a066b83
Karimgamal12 <132415145+Karimgamal12@users.noreply.github.com>
2023-11-04 07:43:50 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int("15170853")
+API_HASH = ("33f17f7ccfa43fd779489dca8ab67e34")
+
+BOT_TOKEN = ("6855066018:AAFVun_hKEVU0XI3Ix1YcZHU9uAPHr-C5nQ")
+OWNER_ID = int("5184436120")
+
+MONGO_DB_URI = ("mongodb+srv://islam2005:islam2005@cluster0.rdjot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+MUST_JOIN =("SOURSE_Al_Rady")
